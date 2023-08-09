b8bca0d2fbf5e731841dc4492f1fd45898b65847
koralle <mkhd7930@gmail.com>
2023-08-09 23:55:15 +0900

    add: chapter2

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res, next) => {
+  res.end("Top page");
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+})
